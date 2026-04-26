6befafee413699f32e8e5378ba5d1b8ce19be6ff
Anatolii Fesiuk <anatolii.fesiuk@gmail.com>
2026-04-26 15:23:09 +0300

    Remove text from logo, show only logo.png

--- client/src/components/Logo.jsx
+++ client/src/components/Logo.jsx
@@ -2,7 +2,6 @@ export default function Logo() {
   return (
     <div className="logo">
       <img className="logo-mark" src="/logo.png" alt="vorDBeste" />
-      vor<span>DB</span>este
     </div>
   );
 }
